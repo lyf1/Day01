a72a1825a67f64150219a4178fe847058010f3b3
李岳峰 <212983670@qq.com>
2017-10-30 15:53:00 +0800

    add Day01

--- MainActivity.java
+++ MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //开始敲代码
+        //我敲了
     }
 }
